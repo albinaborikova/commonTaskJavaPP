4a6495591a60bb4cf44564ad387dc9072f91069a
albinaborikova <108988893+albinaborikova@users.noreply.github.com>
2023-01-18 03:33:32 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Window chooser = new Window("Smart Calculator");
+        chooser.pack();
+        chooser.setSize(800, 400);
+        chooser.setLocation(400, 50);
+        chooser.setVisible(true);
+    }
+}
